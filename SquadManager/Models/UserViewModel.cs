a06663ecf348fc2938b1c68561e388863c38b677
Renan Augusto Ferreira <88636863+RenanAugustoKwn@users.noreply.github.com>
2023-05-04 01:20:01 -0300

    login

---
+++ SquadManager/Models/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SquadManager.Models
+{
+    public class UserViewModel
+    {
+        public string username { get; set; }
+        public string email { get; set; }
+        public string passaword { get; set; }
+
+    }
+}
