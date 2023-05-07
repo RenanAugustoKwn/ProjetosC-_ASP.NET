fd9709f0d9b5dfd5df5824d91d660c007eb6f12a
Renan Augusto Ferreira <88636863+RenanAugustoKwn@users.noreply.github.com>
2023-05-07 12:20:26 -0300

    API Autentication

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IActionResult Login(string username, string password)
+        {
+            return Ok(new { response = "Está Logado"});
+        }
+    }
+}
